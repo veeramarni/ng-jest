4fa5cb064d7685ef895d84e3a7e9cf57b4fc080d
Pieter De vriendt <pieter.devriendt@codit.eu>
2017-04-05 16:50:25 +0200

    initial

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser'
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+  it(`should create object {name: 'test'}`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    expect(app.create()).toEqual({name: 'test'});
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.query(By.css('h1'));
+    console.log(compiled.nativeElement.textContent);
+    expect(compiled.nativeElement.textContent).toContain('Hello from Angular App with Webpack');
+  }));
+})
